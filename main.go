1a074275e3b46ce9c5bf665802ead0697879ef09
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-09-29 17:21:54 -0400

    added mux router

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	log.Println("Setting up JWT microservice!")
 
-	http.HandleFunc("/signin", Signin)
-	http.HandleFunc("/welcome", Welcome)
-	http.HandleFunc("/refresh", Refresh)
+	router := mux.NewRouter()
+	router.HandleFunc("/signin", Signin).Methods("POST")
+	router.HandleFunc("/welcome", Welcome).Methods()
+	router.HandleFunc("/refresh", Refresh).Methods()
 
 	log.Println("Starting JWT microservice!")
 	log.Fatal(http.ListenAndServe(":8080", nil))
